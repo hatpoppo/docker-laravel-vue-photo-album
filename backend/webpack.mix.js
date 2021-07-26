1b7553b7ce07adccc699b969bad2f9e06dcd0bd8
hatpoppo <toshi.hat@gmail.com>
2021-07-26 10:36:44 +0900

    merge

--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -11,7 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.browserSync({
+    proxy: 'localhost:80', // アプリの起動アドレス
+    open: false // ブラウザを自動で開かない
+})
+    .js('resources/js/app.js', 'public/js')
+    // .postCss('resources/css/app.css', 'public/css', [
+    //     //
+    // ])
+    .version()
