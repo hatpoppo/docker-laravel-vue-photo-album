87ae616ecfb61eb80486770e9f034031c3a6ee6f
hatpoppo <toshi.hat@gmail.com>
2021-07-28 11:32:07 +0900

    データ変数の紐付けと送信メソッドの仮実装

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -82,8 +82,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   data: function data() {
     return {
-      tab: 1
+      tab: 1,
+      loginForm: {
+        email: '',
+        password: ''
+      }
     };
+  },
+  methods: {
+    login: function login() {
+      console.log(this.loginForm);
+    }
   }
 });
 
@@ -622,7 +631,73 @@ var render = function() {
         ],
         staticClass: "panel"
       },
-      [_vm._m(0)]
+      [
+        _c(
+          "form",
+          {
+            staticClass: "form",
+            on: {
+              submit: function($event) {
+                $event.preventDefault()
+                return _vm.login.apply(null, arguments)
+              }
+            }
+          },
+          [
+            _c("label", { attrs: { for: "login-email" } }, [_vm._v("Email")]),
+            _vm._v(" "),
+            _c("input", {
+              directives: [
+                {
+                  name: "model",
+                  rawName: "v-model",
+                  value: _vm.loginForm.email,
+                  expression: "loginForm.email"
+                }
+              ],
+              staticClass: "form__item",
+              attrs: { type: "text", id: "login-email" },
+              domProps: { value: _vm.loginForm.email },
+              on: {
+                input: function($event) {
+                  if ($event.target.composing) {
+                    return
+                  }
+                  _vm.$set(_vm.loginForm, "email", $event.target.value)
+                }
+              }
+            }),
+            _vm._v(" "),
+            _c("label", { attrs: { for: "login-password" } }, [
+              _vm._v("Password")
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              directives: [
+                {
+                  name: "model",
+                  rawName: "v-model",
+                  value: _vm.loginForm.password,
+                  expression: "loginForm.password"
+                }
+              ],
+              staticClass: "form__item",
+              attrs: { type: "password", id: "login-password" },
+              domProps: { value: _vm.loginForm.password },
+              on: {
+                input: function($event) {
+                  if ($event.target.composing) {
+                    return
+                  }
+                  _vm.$set(_vm.loginForm, "password", $event.target.value)
+                }
+              }
+            }),
+            _vm._v(" "),
+            _vm._m(0)
+          ]
+        )
+      ]
     ),
     _vm._v(" "),
     _c(
@@ -648,28 +723,12 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("form", { staticClass: "form" }, [
-      _c("label", { attrs: { for: "login-email" } }, [_vm._v("Email")]),
-      _vm._v(" "),
-      _c("input", {
-        staticClass: "form__item",
-        attrs: { type: "text", id: "login-email" }
-      }),
-      _vm._v(" "),
-      _c("label", { attrs: { for: "login-password" } }, [_vm._v("Password")]),
-      _vm._v(" "),
-      _c("input", {
-        staticClass: "form__item",
-        attrs: { type: "password", id: "login-password" }
-      }),
-      _vm._v(" "),
-      _c("div", { staticClass: "form__button" }, [
-        _c(
-          "button",
-          { staticClass: "button button--inverse", attrs: { type: "submit" } },
-          [_vm._v("login")]
-        )
-      ])
+    return _c("div", { staticClass: "form__button" }, [
+      _c(
+        "button",
+        { staticClass: "button button--inverse", attrs: { type: "submit" } },
+        [_vm._v("login")]
+      )
     ])
   }
 ]
