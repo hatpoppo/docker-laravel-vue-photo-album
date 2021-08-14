fb56e82debab53766e51d685e22ef2d37d41ca9c
hatpoppo <toshi.hat@gmail.com>
2021-08-14 19:03:33 +0900

    ローディング

--- backend/resources/js/components/PhotoForm.vue
+++ backend/resources/js/components/PhotoForm.vue
@@ -1,7 +1,10 @@
 <template>
   <div v-show="value" class="photo-form">
     <h2 class="title">Submit a photo</h2>
-    <form class="form" @submit.prevent="submit">
+    <div v-show="loading" class="panel">
+      <Loader>Sending your photo..</Loader>
+    </div>
+    <form v-show="!loading" class="form" @submit.prevent="submit">
       <div class="errors" v-if="errors">
         <ul v-if="errors.photo">
           <li v-for="msg in errors.photo" :key="msg">{{ msg }}</li>
@@ -20,7 +23,12 @@
 
 <script>
 import { CREATED, UNPROCESSABLE_ENTITY } from "../util";
+import Loader from "./Loader.vue";
+
 export default {
+  components: {
+    Loader,
+  },
   props: {
     value: {
       type: Boolean,
@@ -29,6 +37,7 @@ export default {
   },
   data() {
     return {
+      loading: false,
       preview: null,
       photo: null,
       errors: null,
@@ -55,10 +64,12 @@ export default {
       this.$el.querySelector('input[type="file"]').value = null;
     },
     async submit() {
+      this.loading = true;
       const formData = new FormData();
       formData.append("photo", this.photo);
       const response = await axios.post("/api/photos", formData);
 
+      this.loading = false;
       if (response.status === UNPROCESSABLE_ENTITY) {
         this.errors = response.data.errors;
         return false;
@@ -70,7 +81,7 @@ export default {
       if (response.status !== CREATED) {
         this.$store.commit("error/setCode", response.status);
       }
-      this.$router.push("/photos/${response.data.i}");
+      this.$router.push(`/photos/${response.data.id}`);
     },
   },
 };
